28403a91034f1c0c66fedf836db1e6b9eaea45fd
mariyo <mario.palka@gmail.com>
2023-12-06 13:26:48 +0100

    citanie day1 input

--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -4,8 +4,9 @@ import kotlin.test.assertEquals
 class Day01Test {
 
     @Test
-    fun `hello method should return a proper greeting`() {
-        assertEquals("Hello unit hello.", "Hello unit hello.")
+    fun `it can read input from resources`() {
+        val input = readInput("Day01_input");
+        assertEquals(1000, input.size)
     }
 
 }
