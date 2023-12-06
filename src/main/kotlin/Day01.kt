ceab1aeb64d526979392c0bbae5e0fed0f312a49
mariyo <mario.palka@gmail.com>
2023-12-06 21:53:27 +0100

    day1 part1 uspesny vysledok = 54916

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -15,3 +15,36 @@ fun main() {
     part1(input).println()
     part2(input).println()
 }
+
+class Day01 {
+    fun part1SumCalibrationValuesFromInput(input: List<String>): Int {
+        var result = 0
+
+        input.forEach { oneLine ->
+            var first: Char? = null
+            var last: Char? = null
+
+            oneLine.forEach { oneChar ->
+                if (oneChar.isDigit()) {
+                    if (first == null) first = oneChar else last = oneChar
+                }
+            }
+
+            if (first == null) {
+                throw RuntimeException("Invalid input line")
+            }
+
+            if (last == null) {
+                last = first
+            }
+
+            result += ("$first$last").toInt()
+        }
+
+        return result
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,12 +1,56 @@
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class Day01Test {
-
     @Test
     fun `it can read input from resources`() {
-        val input = readInput("Day01_input");
+        val input = readInput("Day01_input")
         assertEquals(1000, input.size)
     }
 
+    @TestFactory
+    fun `it can solve basic examples`() = listOf(
+        "1abc2" to 12,
+        "pqr3stu8vwx" to 38,
+        "a1b2c3d4e5f" to 15,
+        "treb7uchet" to 77,
+    ).map { (input, expected) ->
+        DynamicTest.dynamicTest("it can solve $input to $expected") {
+            assertEquals(
+                expected,
+                Day01().part1SumCalibrationValuesFromInput(
+                    listOf(input)
+                )
+            )
+        }
+    }
+
+    @Test
+    fun `it can sum multiple lines of input`() {
+        assertEquals(
+            142,
+            Day01().part1SumCalibrationValuesFromInput(
+                listOf(
+                    "1abc2",
+                    "pqr3stu8vwx",
+                    "a1b2c3d4e5f",
+                    "treb7uchet",
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `it can sum input from Day01_input resource`() {
+        val day01Part1Result = Day01().part1SumCalibrationValuesFromInput(
+            readInput("Day01_input")
+        )
+
+        assertTrue(0 <= day01Part1Result)
+
+        println(day01Part1Result)
+    }
 }
